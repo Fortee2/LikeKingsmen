71778bf6b5ddfad637e93357aaa9f9a60f57f430
Randy Costner <randy.costner@kingsmensoftware.com>
2023-05-23 17:56:18 -0400

    Added 15 second pause

--- background.js
+++ background.js
@@ -12,84 +12,36 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   }
 });
 
-
 function likeAndRepostForKingsmen() {
-  // Select all parent divs
-  const parentDivs = document.querySelectorAll('div.ember-view');
+    setTimeout(() =>{
+       // Select all parent divs
+      const parentDivs = document.querySelectorAll('div.ember-view');
 
-  parentDivs.forEach((div) => {
-    // Check for an unclicked like button within this div
-    const likeButton = div.querySelector(
-      'button[aria-label="React Like to Kingsmen Software’s post"][aria-pressed="false"]'
-    );
-
-    if (likeButton) {
-      // If found, click the like button
-      likeButton.click();
-
-      // Check for a repost button within this div
-      const repostButton = div.querySelector('button[aria-expanded="false"] li-icon[type="repost"]');
-      if (repostButton) {
-        // If found, click the repost button
-        repostButton.click();
-
-        // Wait for the dropdown to appear and click on the div
-        waitForElement('.social-reshare-button__sharing-as-is-dropdown-item-icon')
-          .then((element) => {
-            const parentDiv = element.parentElement;
-            parentDiv.click();
-          });
-      }
-    }
-  });
-}
-
-function repostForKingsmen() {
-  const buttons = document.querySelectorAll('button[aria-expanded="false"]');
-  const repostButtons = [];
-
-  buttons.forEach((button) => {
-    const liIcon = button.querySelector('li-icon[type="repost"]');
-    if (liIcon) {
-      repostButtons.push(button);
-      console.log( button.innerHTML);
-    }
-  });
-
-  for (const repostButton of repostButtons) {
-    repostButton.click();
-    const htmlarray = document.querySelectorAll(
-      'div[class="artdeco-dropdown__content artdeco-dropdown__content--is-open artdeco-dropdown--is-dropdown-element artdeco-dropdown__content--justification-right artdeco-dropdown__content--placement-bottom ember-view social-reshare-button__share-dropdown-content"]'
-    );
-
-    htmlarray.forEach((html) => {
-      html.click();
-    });
-  }
-}
-
-function waitForElement(selector) {
-  return new Promise((resolve) => {
-    const element = document.querySelector(selector);
-    if (element) {
-      resolve(element);
-      return;
-    }
-
-    const observer = new MutationObserver((mutations) => {
-      for (const mutation of mutations) {
-        for (const addedNode of mutation.addedNodes) {
-          if (addedNode.matches && addedNode.matches(selector)) {
-            resolve(addedNode);
-            observer.disconnect();
-            return;
+      for (const element of parentDivs) {
+        const div = element;
+        // Check for an unclicked like button within this div
+        const likeButton = div.querySelector(
+          'button[aria-label="React Like to Kingsmen Software’s post"][aria-pressed="false"]'
+        );
+      
+        if (likeButton) {
+          // If found, click the like button
+          likeButton.click();
+          console.log('like button clicked');
+      
+           // Check for a repost button within this div
+          const repostButton = div.querySelector('button[aria-expanded="false"] li-icon[type="repost"]');
+          if (repostButton) {
+            // If found, click the repost button
+            repostButton.click();
+            console.log('repost button clicked');
           }
+          break; // This will break the loop
         }
       }
-    });
 
-    observer.observe(document.body, { childList: true, subtree: true });
-  });
+    }, 15000);
+
 }
 
 async function getActiveTab() {
